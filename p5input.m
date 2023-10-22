e18f424f2ab4cd62470852143b90bdda2f66a9c1
allison714 <76913628+allison714@users.noreply.github.com>
2023-10-22 00:22:39 -0400

    Update p5input.m

--- p5input.m
+++ p5input.m
@@ -3,7 +3,7 @@ function promptAns = P5input
     dlgtitle = 'Imaging Analysis Input Parameters'; % change this when new params are added
     boxDims = [1 50; 1 50; 1 50; 1 50; 1 50];
     
-    presetInput = {'LC14', 'GC7b', 'Allison', '', '[13:15, 17:33, 35:40]'};
+    presetInput = {'LC14', 'GC7b', 'Allison', '', '[1:500]'};
     
     stimDDopts = {'AllisonContrastBars01', 'Bars_Abrupt_001', 'Bars_AC_03', 'sweepingFullBars_4D_5reps', 'TargetSweep_singleNeuronRFMap_5degreso_40degArea_nogrid_10degTarg', 'vsweep_02','vsweep15_480','WaveFlashBarObjectLoom_x20y-20_3rep'};
     stimIndex = listdlg('PromptString', 'Select Param Name:', 'SelectionMode', 'single', 'ListString', stimDDopts);
